a61e2c345f778706c23616743686102775502f5d
Mathieu Halpin <97577530+mathieuhalpin@users.noreply.github.com>
2022-10-31 00:48:45 -0500

    fixed lol

---
+++ assignments/assignment10/assignment10main.js
@@ -0,0 +1,35 @@
+// Problem 1
+
+{
+    employees[
+        {"firstName": "Sam", "department": "Tech", "designation": "Manager", salary: 4000, raiseEligible: true, wfh: true},
+        {"firstName": "Mary", "department": "Finance", "designation": "Trainee", salary: 18500, raiseEligible: true, wfh: false},
+        {"firstName": "Bill", "department": "HR", "designation": "Executive", salary: 21200, raiseEligible: false, wfh: false},
+        {"firstName": "Anna", "department": "Tech", "designation": "Executive", salary: 25600, raiseEligible: false, wfh: true}
+    ]
+}
+// Problem 2
+{
+    company[
+        {"companyName": "Tech Stars", "website": "techstars.com", employees}
+    ]
+}
+
+// Problem 4
+
+function AddSalaries(salary) {
+    totalSalary = 0;
+    for (let i of salary) {
+        totalSalary += salary;
+    }
+    return totalSalary;
+}
+
+// Problem 5
+
+if (raiseEligible = true) {
+    salary = salary * 1.1
+}
+if (raiseEligible = false) {
+    salary = salary
+}
