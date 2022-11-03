b94a43e8d7b02a9bb35a85f64200791b376d2ff5
Mathieu Halpin <97577530+mathieuhalpin@users.noreply.github.com>
2022-11-02 22:01:10 -0600

    fixed with try and error catch

--- assignments/ica11/ica11main.js
+++ assignments/ica11/ica11main.js
@@ -5,17 +5,22 @@ quoteButton.addEventListener('click', getQuote);
 window.addEventListener('reload', getQuote);
 
 async function getQuote() {
-    console.log('getQuote works');
-    let text = await fetch(end);
-    let respond = await text.text();
-    let json_response = JSON.parse(respond);
-    console.log(json_response);
-    console.log(json_response['message']);
-
-    displayQuote(json_response['message']);
+    try {
+      console.log("getQuote works");
+      let text = await fetch(end);
+      let respond = await text.text();
+      let json_response = JSON.parse(respond);
+      console.log(json_response);
+      console.log(json_response["message"]);
+  
+      displayQuote(json_response["message"]);
+    } 
+    catch (err) {
+      console.log(err);
+      alert(err);
+    }
 }
 
-function displayQuote() {
-
-    a = document.getElementById('js-quote-text').textContent;
+function displayQuote(quote) {
+    document.getElementById("js-quote-text").textContent = quote;
 }
