16a8d4368ba69b8e1d122e5f60bce84771c4a58d
Mathieu Halpin <97577530+mathieuhalpin@users.noreply.github.com>
2022-11-06 22:52:33 -0700

    final fix

--- assignments/assignment11/assignment11main.js
+++ assignments/assignment11/assignment11main.js
@@ -1,24 +1,27 @@
 const movieButton = document.querySelector('.new-movie');
-const end = 'https://ghibliapi.herokuapp.com/films/{id}';
+const end = 'https://ghibliapi.herokuapp.com/films';
 
 movieButton.addEventListener('click', getFilm);
 window.addEventListener('reload', getFilm);
 
 async function getFilm() {
-    try {
-      console.log("getFilm works");
-      let text = await fetch(end);
-      let respond = await text.text();
-      let json_response = JSON.parse(respond);
-      console.log(json_response);
-      console.log(json_response["message"]);
-  
-      displayFilm(json_response["message"]);
-    } 
-    catch (err) {
-      console.log(err);
-      alert(err);
-    }
+  try {
+    console.log("getFilm works");
+    let text = await fetch(end);
+    let respond = await text.text();
+    let json_response = JSON.parse(respond);
+
+    const randomFilm =
+      json_response[Math.floor(Math.random() * json_response.length)];
+
+    console.log(randomFilm.title);
+
+    displayFilm(randomFilm.title);
+  } 
+  catch (err) {
+    console.log(err);
+    alert(err);
+  }
 }
 
 function displayFilm(film) {
