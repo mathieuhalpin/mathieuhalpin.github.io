4c24c46cb2465fd1f0f55a10e288c0eaeed6d49d
Mathieu Halpin <97577530+mathieuhalpin@users.noreply.github.com>
2022-11-06 19:35:29 -0700

    assignment11 + new css test?

---
+++ assignments/assignment11/assignment11main.js
@@ -0,0 +1,26 @@
+const quoteButton = document.querySelector('.new-movie');
+const end = 'https://ghibliapi.herokuapp.com/films/{id}';
+
+quoteButton.addEventListener('click', getMovie);
+window.addEventListener('reload', getMovie);
+
+async function getMovie() {
+    try {
+      console.log("getMovie works");
+      let text = await fetch(end);
+      let respond = await text.text();
+      let json_response = JSON.parse(respond);
+      console.log(json_response);
+      console.log(json_response["message"]);
+  
+      displayMovie(json_response["message"]);
+    } 
+    catch (err) {
+      console.log(err);
+      alert(err);
+    }
+}
+
+function displayQuote(movie) {
+    document.getElementById("js-movie-text").textContent = quote;
+}
