6089c33bb9f1b148dbe1f625361785f369050bc2
Mathieu Halpin <97577530+mathieuhalpin@users.noreply.github.com>
2022-11-09 18:34:59 -0700

    wa12

--- quiz/wa/wa12.js
+++ quiz/wa/wa12.js
@@ -1,4 +1,4 @@
-const button = document.querySelector('.button');
+/*const button = document.querySelector('.button');
 const output = document.querySelector('.output');
 let phone_content = document.querySelector('.phone');
 
@@ -6,4 +6,59 @@ button.addEventListener('click', updateOutput);
 
 function updateOutput() {
     output.textContent = phone_content.value;
+}
+*/
+
+const minusButton = document.querySelector('.minus-button').addEventListener('click', minus);
+const plusButton = document.querySelector('.plus-button').addEventListener('click', plus);
+const resetButton = document.querySelector('.reset-button').addEventListener('click', reset);
+const randomButton = document.querySelector('.random-button').addEventListener('click', random);
+const submitButton = document.querySelector('.submit-button').addEventListener('click', submit);
+
+function check(){
+    console.log('test');
+}
+
+function submit(){
+    alert(output.textContent);
+}
+
+function reset(){
+    outputInt = 0;
+    output.textContent = outputInt;
+    
+}
+
+function plus(){
+    if (outputInt < 9999999999){
+        outputInt ++;
+        output.textContent = outputInt;
+    }
+}
+
+
+function minus(){
+    if (outputInt >0){
+        outputInt --;
+        output.textContent = outputInt;
+    }
+}
+
+function randomNumber(min, max){
+    const num = Math.floor(Math.random() * (max - min + 1) + min)
+    return num;
+}
+
+function random(){
+    outputInt = randomNumber(0, 9999999999);
+    output.textContent = outputInt;
+}
+
+//Slider
+var slider = document.getElementById("myRange");
+const sliderSubmit = document.querySelector(".sliderSubmit").addEventListener('click', update);
+var sliderOutput = document.querySelector(".slider-output");
+
+function update(){
+    sliderOutput.textContent = slider.value;
 }
