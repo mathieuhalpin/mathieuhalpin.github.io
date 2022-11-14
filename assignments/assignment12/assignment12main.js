d9801a7059fcb0942f84bf4a8b05574cba81a540
Mathieu Halpin <97577530+mathieuhalpin@users.noreply.github.com>
2022-11-13 22:14:38 -0700

    hopefully done

--- assignments/assignment12/assignment12main.js
+++ assignments/assignment12/assignment12main.js
@@ -4,8 +4,8 @@ let phone_content = document.querySelector('.phone');
 
 button.addEventListener('click', updateOutput);
 
-const yesButton = document.querySelector('.yes-button').addEventListener('click', yes);
-const noButton = document.querySelector('.no-button').addEventListener('click', no);
+const yesButton = document.querySelector('.yes-button').addEventListener('click', yesButtonClicked);
+const noButton = document.querySelector('.no-button').addEventListener('click', noButtonClicked);
 
 function updateOutput() {
     output.textContent = phone_content.value;
@@ -18,11 +18,20 @@ async function getNumber() {
     const getNumString = n => new Array(n).fill(0).map(() => randNum()).join('');
     const generateNumber = () => '${getNumString(3)}-${getNumString(3)}-${getNumString(4)}';
 
-    if(confirm("Is ", generateNumber, "your number?") == true) {
-        response = "Thank you for entering your number!";
-    }
-    else{
-        
-    }
-
 }
+
+function displayNumber(generateNumber) {
+    document.querySelector(".number-text").textContent = "Is ", generateNumber, " your number?";
+}
+
+function yesButtonClicked() {
+    document.querySelector(".number-text").textContent = "Thank you for entering your number!";
+}
+
+function noButtonClicked() {
+    document.querySelector(".number-text").textContent = "Please refresh the page to see if your number is next!"
+}
+
+
+
+getNumber();
