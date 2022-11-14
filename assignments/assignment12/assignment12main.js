0da44d581577d4cb86d2c37dade94a288aaa41f4
Mathieu Halpin <97577530+mathieuhalpin@users.noreply.github.com>
2022-11-13 22:29:43 -0700

    streamlined and fixed

--- assignments/assignment12/assignment12main.js
+++ assignments/assignment12/assignment12main.js
@@ -1,37 +1,36 @@
-const button = document.querySelector('.button');
-const output = document.querySelector('.output');
-let phone_content = document.querySelector('.phone');
+const yesButton = document
+  .querySelector(".yes-button")
+  .addEventListener("click", yesButtonClicked);
+const noButton = document
+  .querySelector(".no-button")
+  .addEventListener("click", noButtonClicked);
 
-button.addEventListener('click', updateOutput);
+function getNumber() {
+  const randNum = () => Math.floor(Math.random() * 10);
+  const getNumString = (n) =>
+    new Array(n)
+      .fill(0)
+      .map(() => randNum())
+      .join("");
+  const generateNumber = () =>
+    `${getNumString(3)}-${getNumString(3)}-${getNumString(4)}`;
 
-const yesButton = document.querySelector('.yes-button').addEventListener('click', yesButtonClicked);
-const noButton = document.querySelector('.no-button').addEventListener('click', noButtonClicked);
-
-function updateOutput() {
-    output.textContent = phone_content.value;
-}
-
-
-async function getNumber() {
-    let response;
-    const randNum = () => Math.floor(Math.random()*10);
-    const getNumString = n => new Array(n).fill(0).map(() => randNum()).join('');
-    const generateNumber = () => '${getNumString(3)}-${getNumString(3)}-${getNumString(4)}';
-
+  displayNumber(generateNumber());
 }
 
 function displayNumber(generateNumber) {
-    document.querySelector(".number-text").textContent = "Is ", generateNumber, " your number?";
+  document.querySelector(".number-text").textContent =
+    "Is " + generateNumber + " your number?";
 }
 
 function yesButtonClicked() {
-    document.querySelector(".number-text").textContent = "Thank you for entering your number!";
+  document.querySelector(".number-text").textContent =
+    "Thank you for entering your number!";
 }
 
 function noButtonClicked() {
-    document.querySelector(".number-text").textContent = "Please refresh the page to see if your number is next!"
+  document.querySelector(".number-text").textContent =
+    "Please refresh the page to see if your number is next!";
 }
-
-
 
 getNumber();
